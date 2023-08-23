96aebe364e98fbba0cc777172ffa0bf7004b3255
CoolyMilk <141494583+CoolyMilk@users.noreply.github.com>
2023-08-23 11:37:50 +0800

    Update merge_sort.c
    Commented it!

--- merge_sort.c
+++ merge_sort.c
@@ -3,23 +3,33 @@
 
 // Sorry if some stuff looks weird, I had to shave off some space but it wasn't enough
 void no_recursion_merge_sort(int *array, int size) {
-    int holder[size], marker[size], i, j, length = size;
+    int holder[size], marker[size], length = size; // Stuff to simulate 'recursion'
     
-    for (i = 0; i<size; i++) { 
+    for (int i = 0; i<size; i++) { 
       marker[i] = 1; 
-    }
+    } // Setting up every individual element as 'arrays' of one
+    // Marker just keep tracks of 'sub arrays'
     
-    while (length > 1) {
+    while (length > 1) { // I think it's just faster to do it this way instead of calculating how much iterations is required using square root
         int passes = length/2, location = 0;
+
+        // Passes is just how much arrays left after combining it
+        // There is a case where an odd array won't be paired
+        // But it's fixed at the end of the iteration and the number of arrays 
+        // at the next pass will be even now
         
-        for (i = 0; i < passes; i++) {
+        for (int i = 0; i < passes; i++) {
+            // Skipping indexes to pair them by two
             int real_i = i*2;
             int total_length = marker[real_i] + marker[real_i+1];
-            
+
+            // Making indexes to simulate the merge thing
             int left_start = location, left_end = location + marker[real_i];
             int right_start = left_end, right_end = location + total_length;
-            
-            for (j = 0; j < total_length; j++) {
+
+            // Placing it in holder so it doesn't write over itself
+            // Mergeeee!!
+            for (int j = 0; j < total_length; j++) {
                 if (left_start >= left_end) {
                     holder[j] = array[right_start];
                     right_start++;
@@ -39,14 +49,17 @@ void no_recursion_merge_sort(int *array, int size) {
                     }
                 }
             }
-            
-            for (j = 0; j < total_length; j++) { 
+            // Transferring the sorted array back into the original!
+            for (int j = 0; j < total_length; j++) { 
                 array[location+j] = holder[j]; 
             }
+            // Updating the new array in marker
             location += total_length;
             marker[i] = total_length;
         }
-        
+
+        // It will scoot the not paired array if it exists
+        // Back next with the others so it doesn't get forgotten
         if (passes*2 != length) {
             marker[passes] = marker[length-1];
             length = passes+1;
@@ -57,6 +70,32 @@ void no_recursion_merge_sort(int *array, int size) {
     }
 
 }
+
+/*
+
+Basically
+(I just replaced the unused 1s with 0s
+so you won't get confused, but in reality
+they are ones that just sit there)
+
+holder = {1, 1, 1, 1, 1};
+array  = {4, 3, 2, 1, 0};
+
+holder = {2, 2, 1, 0, 0}; 
+array  = {3, 4, 1, 2, 0};
+
+holder = {4, 1, 0, 0, 0};
+array  = {1, 2, 3, 4, 0};
+
+holder = {5, 0, 0, 0, 0};
+array  = {0, 1, 2, 3, 4};
+
+Sorted!!
+not sure if it's 100% sorted for all cases though
+but if it works in the small scale, it should work in the
+big scale?
+*/
+
 
 int main(int argc, char **argv) {
   	int test[] = {123, 93729, 298392, 0, -293, 3927, 23842};
@@ -69,4 +108,5 @@ int main(int argc, char **argv) {
     	printf("%d, ", test[i]);
     } 
     printf("]\n");
+}
     
